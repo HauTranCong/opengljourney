8310774b1cd0288344e143b35827dc71b21bee88
HauTranCong <haunek22@gmail.com>
2024-07-20 16:02:51 +0000

    learn about textures in OpenGL

--- 3.Textures/TriangleWithMoreAttributes.cpp
+++ 3.Textures/TriangleWithMoreAttributes.cpp
@@ -110,7 +110,8 @@ int main()
     // load image, create texture and generate mipmaps
     int width, height, nrChannels;
     std::string texturePath = std::string(GLSL_PATH) + "/Shader/container.jpg";
-    std::cout << "texture path: " << texturePath << std::endl; 
+    std::cout << "texture1 path: " << texturePath << std::endl; 
+    stbi_set_flip_vertically_on_load(true); 
     unsigned char *data = stbi_load(texturePath.c_str(), &width, &height, &nrChannels, 0);
     if (data)
     {
@@ -124,6 +125,36 @@ int main()
     stbi_image_free(data);
 
 
+    // texture 2
+    // ---------
+    unsigned int texture2;
+    glGenTextures(1, &texture2);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    // set the texture wrapping parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // set texture filtering parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // load image, create texture and generate mipmaps
+    std::string texture2Path = std::string(GLSL_PATH) + "/Shader/awesomeface.png";
+    std::cout << "texture2 path: " << texture2Path << std::endl; 
+    data = stbi_load(texture2Path.c_str(), &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        // note that the awesomeface.png has transparency and thus an alpha channel, so make sure to tell OpenGL the data type is of GL_RGBA
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D); 
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+
+    ourShader.use();
+    ourShader.setInt("texture", 0);
+    ourShader.setInt("texture2", 1);
     // render loop
     // -----------
     while (!glfwWindowShouldClose(window))
@@ -137,8 +168,12 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // bind Texture
+        // bind textures on corresponding texture units
+        glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, texture);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
+        
         // render container
         ourShader.use();
         glBindVertexArray(VAO);
